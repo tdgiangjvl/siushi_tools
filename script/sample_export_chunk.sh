5ce7d8f05fd2abfe5979f927cf64cc1670976ffc
Yang <tdgiangjvl@gmail.com>
2024-10-17 23:27:25 +0700

    add sample record url

--- script/sample_export_chunk.sh
+++ script/sample_export_chunk.sh
@@ -1,1 +1,3 @@
+python tiktok-live-recorder/src/main.py -output /mnt/disk2/tiktok_data/raw_vid -url <paste url here>
+
 python script/utils/export_high_volumn.py /mnt/disk2/tiktok_data/raw_vid/TK_quyenleodaily_2024.10.15_21-48-03_flv.mp4 
